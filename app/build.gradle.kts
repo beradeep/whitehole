4b11602a8cabf760e883b5dd7b95e483f5d9a435
Subhradeep <beradeep35@gmail.com>
2024-07-12 18:18:45 +0530

    fix app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bera.whitehole"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
