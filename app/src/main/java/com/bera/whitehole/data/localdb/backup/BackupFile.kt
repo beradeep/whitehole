ffac3756ec84b1cfb0f3d1877e56717a3e276d74
berabros <124783808+beradeep@users.noreply.github.com>
2024-08-29 20:25:30 +0530

    fix import db backup (#19)

--- app/src/main/java/com/bera/whitehole/data/localdb/backup/BackupFile.kt
+++ app/src/main/java/com/bera/whitehole/data/localdb/backup/BackupFile.kt
@@ -1,8 +1,10 @@
 package com.bera.whitehole.data.localdb.backup
 
+import androidx.annotation.Keep
 import com.bera.whitehole.data.localdb.entities.Photo
 import com.bera.whitehole.data.localdb.entities.RemotePhoto
 
+@Keep
 data class BackupFile(
     val photos: List<Photo> = emptyList(),
     val remotePhotos: List<RemotePhoto> = emptyList(),
